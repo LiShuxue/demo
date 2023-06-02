d35d1a523b2d80274e8aec5f886c56387eec8d66
LiShuxue <1149926505@qq.com>
2023-06-02 11:27:48 +0800

    复现问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import { useRef, useState } from 'react';
+import { MdEditor } from 'md-editor-rt';
+import type { ExposeParam } from 'md-editor-rt';
+import 'md-editor-rt/lib/style.css';
 
 const App = () => {
+  const editorRef = useRef<ExposeParam>();
+  const [mdString, setMdString] = useState('');
+
   return (
-    <div className="app">
-      <RouterProvider router={router} fallbackElement={<p>Route Loading...</p>} />
+    <div className="test">
+      <MdEditor
+        ref={editorRef}
+        modelValue={mdString}
+        previewTheme="github"
+        codeTheme="github"
+        onChange={setMdString}
+      />
     </div>
   );
 };
